b7195f554e2bbb9f6f28ffac5b450e61887796ef
Mengqi Li <mengqi1@ad.unc.edu>
2021-09-14 12:39:30 -0400

    update

--- exercises/ex03/happy_trees.py
+++ exercises/ex03/happy_trees.py
@@ -4,13 +4,13 @@ __author__ = "730485647"
 
 # The string constant for the pine tree emoji
 TREE: str = '\U0001F332'
-input: int = int(input("Depth: "))
+depth: int = int(input("Depth: "))
 i: int = 0
 j: int = 1 
 output: str = ""
-if input > 0: 
-    while i < input + 1:
-        while j < input + 1:
+if depth > 0: 
+    while i < depth + 1:
+        while j < depth + 1:
             output = output + TREE
             print(output)
             j = j + 1
