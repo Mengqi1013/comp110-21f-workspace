7c704193604611ae0f9f73d3ace681067d879778
Mengqi Li <mengqi1@ad.unc.edu>
2021-10-01 16:17:08 -0400

    a

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,51 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730485647"
+
 
 # Define your functions below
+def only_evens(xs: list[int]) -> list[int]:
+    """Returns the even elements in a list."""
+    i: int = 0
+    even_list: list[int] = []
+    while i < len(xs): 
+        item: int = 0
+        if xs[i] // 2 == 0:
+            item = item + xs[i]
+            xs.append(item)
+        else:
+            popped_item: int = xs[i]
+            xs.pop(popped_item)
+        i = i + 1
+    return even_list
+
+
+def sub(xs: list[int], a: int, b: int) -> list[int]:
+    """Returns a subset of the given list based on the starting and ending indexes."""
+    new_list: list[int] = []
+    i: int = a
+    while i < (b - a):
+        new_list.append(xs[i])
+        i = i + 1
+    return new_list
+
+
+def concat(xs: list[int], ys: list[int]) -> list[int]:
+    """Concatenates two lists given and returns a new list."""
+    i: int = 0 
+    string1: str = ""
+    string2: str = ""
+    new_list: list[int] = []
+    while i < len(xs):
+        string1 = str(xs[i])
+        new_list.append(int(string1[i]))
+        i = i + 1
+    while i < len(ys):
+        string2 = str(ys[i])
+        new_list.append(int(string2[i]))
+        i = i + 1   
+    return new_list
+
+
+print(concat([1, 2, 3], [4, 5, 6]))
+            
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,27 @@
 """Unit tests for list utility functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
+from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730485647"
+
+
+def test_only_evens():
+    """Tests if the only_evens function works as expected."""
+    assert only_evens([1, 2, 3]) == [2]
+    assert only_evens([1, 5, 3]) == []
+    assert only_evens([4, 4, 4]) == [4]
+
+
+def test_sub():
+    """Tests if the sub function works as expected."""
+    assert sub([10, 20, 30, 40], 1, 3) == [20, 30]
+    assert sub([1, 4, 8, 2], 0, 3) == [1, 4, 8]
+    assert sub([14, 18, 54, 13, 12, 65, 15], 2, 6) == [54, 13, 12, 65]
+
+
+def test_concat():
+    """Tests if the concat function works as expected."""
+    assert concat([], []) == []
+    assert concat([1, 2, 3], [4, 5, 6]) == [1, 2, 3, 4, 5, 6]
+    assert concat([4, 1], [3, 5]) == [4, 1, 3, 5]
